78f8c7402446142eb76f37bb3ac05320889214d7
Phillip Yoder <phillipmyoder@gmail.com>
2021-05-12 22:31:23 -0400

    the actual code

--- src/index.js
+++ src/index.js
@@ -1,17 +1,97 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+//https://www.youtube.com/watch?v=Y-GkMjUZsmM&t=2242s
+const X_CLASS = 'x'
+const CIRCLE_CLASS = 'circle'
+const WINNING_COMBINATIONS = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+]
+const cellElements = document.querySelectorAll('[data-cell]')
+const board = document.getElementById('board')
+const winningMessageElement = document.getElementById('winningMessage')
+const restartButton = document.getElementById('restartButton')
+const winningMessageTextElement = document.querySelector('[data-winning-message-text]')
+let circleTurn
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+startGame()
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+restartButton.addEventListener('click', startGame)
+
+function startGame(){
+    circleTurn = false
+    cellElements.forEach(cell => {
+        cell.classList.remove(X_CLASS)
+        cell.classList.remove(CIRCLE_CLASS)
+        cell.removeEventListener('click', handleClick)
+        cell.addEventListener('click', handleClick, {once:true})
+    })
+    setBoardHoverClass()
+    winningMessageElement.classList.remove('show')
+}
+
+function handleClick(e){
+    const cell = e.target
+    const currentClass = circleTurn ? CIRCLE_CLASS : X_CLASS
+    placeMark(cell, currentClass)
+    if(checkWin(currentClass)){
+        endGame(false)
+    }
+    else if(isDraw()){
+        endGame(true)
+    }
+    else{
+        swapTurns()
+        setBoardHoverClass()
+    }
+    //check for win
+    //check for draw
+}
+
+function endGame(draw){
+    if(draw){
+        winningMessageTextElement.innerText = 'Draw!'
+    }
+    else{
+        winningMessageTextElement.innerText = `${circleTurn ? "O" : "X"} Win!`
+    }
+    winningMessageElement.classList.add('show')
+}
+
+function isDraw(){
+    return [...cellElements].every(cell => {
+        return cell.classList.contains(X_CLASS) ||
+        cell.classList.contains(CIRCLE_CLASS)
+    })
+}
+
+function placeMark(cell, currentClass){
+    cell.classList.add(currentClass)
+}
+
+function swapTurns(){
+    circleTurn = !circleTurn
+}
+
+function setBoardHoverClass(){
+    board.classList.remove(X_CLASS)
+    board.classList.remove(CIRCLE_CLASS)
+    if(circleTurn){
+        board.classList.add(CIRCLE_CLASS)
+    }
+    else{
+        board.classList.add(X_CLASS)
+    }
+}
+
+function checkWin(currentClass){
+    return WINNING_COMBINATIONS.some(combination => {
+        return combination.every(index => {
+            return cellElements[index].classList.contains(currentClass)
+        })
+    })
+}
